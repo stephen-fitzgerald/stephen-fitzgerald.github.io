5b3f7c695ef722cbd9af75ebfa51ed77738d4183
fitz-tenera <stephen@tenera.care>
2022-09-23 15:07:43 -0300

    custom element and css play

---
+++ zzz-ref/my-element.mjs
@@ -0,0 +1,53 @@
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+    // element created
+  }
+
+  render() {
+    let date = new Date(this.getAttribute('datetime') || Date.now());
+
+    this.innerHTML = new Intl.DateTimeFormat("default", {
+      year: this.getAttribute('year') || undefined,
+      month: this.getAttribute('month') || undefined,
+      day: this.getAttribute('day') || undefined,
+      hour: this.getAttribute('hour') || undefined,
+      minute: this.getAttribute('minute') || undefined,
+      second: this.getAttribute('second') || undefined,
+      timeZoneName: this.getAttribute('time-zone-name') || undefined,
+    }).format(date);
+  }
+  connectedCallback() {
+    // browser calls this method when the element is added to the document
+    // (can be called many times if an element is repeatedly added/removed)
+    if( !this.rendered){
+      this.render();
+      this.rendered = true;
+    }
+  }
+
+  disconnectedCallback() {
+    // browser calls this method when the element is removed from the document
+    // (can be called many times if an element is repeatedly added/removed)
+  }
+
+  static get observedAttributes() {
+    /* array of attribute names to monitor for changes */
+    return ['datetime', 'year', 'month', 'day', 'hour', 'minute', 'second', 'time-zone-name'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // called when one of attributes listed above is modified
+    this.render();
+  }
+
+  adoptedCallback() {
+    // called when the element is moved to a new document
+    // (happens in document.adoptNode, very rarely used)
+  }
+
+  // there can be other element methods and properties
+}
+
+customElements.define("my-element", MyElement);
+setInterval(() => elem.setAttribute('datetime', new Date()), 1000);
