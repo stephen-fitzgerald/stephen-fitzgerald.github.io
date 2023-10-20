0809de713d4e5416ca81b4487fd05d1326d923fb
fitz-tenera <stephen@tenera.care>
2023-10-19 21:48:22 -0300

    bat compression vs pressure

--- js/pci/util/dom.mjs
+++ js/pci/util/dom.mjs
@@ -1,4 +1,21 @@
-
+/**
+ * Builds an object with keys matching element ids in the 
+ * parent element, or the document.  IDs are coverted to camel case.
+ * Values are the references themselves.
+ * 
+ * Usage:
+ * 
+ *  <div id=my-element></div>
+ * 
+ *  let doc = getDomRefsById();
+ * 
+ *  doc.myElement.addEventListener(...)
+ * 
+ * @export
+ * @param {any} [parentElement=document] 
+ * @returns {object} object with keys = camel case ids and values
+ * equal to the elements reference.
+ */
 export function getDomRefsById(parentElement = document) {
     let $refs = {};
     parentElement.querySelectorAll('[id]').forEach($el => {
