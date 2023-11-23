77bacd63b2d334161ca5961163a760da372ae0d1
fitz-tenera <stephen@tenera.care>
2023-11-23 11:06:58 -0400

    added comments

--- components/molded-tube-wc/molded-tube-wc.mjs
+++ components/molded-tube-wc/molded-tube-wc.mjs
@@ -10,135 +10,185 @@ class MoldedTubeWC extends HTMLElement {
 
   constructor() {
     super();
-
     this.initialized = false;
-    this.canvasHeight = 600;
-    this.canvasWidth = 300;
 
     // Attach a shadow root to the element.
     let shadowRoot = this.attachShadow({ mode: 'open' });
 
-    // element created
-  }
+    // element created, set some default values
 
-  connectedCallback() {
-    // browser calls this method when the element is added to the document
-    // (can be called many times if an element is repeatedly added/removed)
+    this.canvasHeight = 600;
+    this.canvasWidth = 300;
 
-    this._moldedTube = getTube();
-    this._profile = this.moldedTube.profile;
-    this._plySpecs = this.moldedTube.plySpecs;
-
-    this._zoomIntensity = 0.15;
-
-    // extents is in real worlld units = meters
-    this._extents = {
-      minX: this._profile.xMin,
-      maxX: this._profile.xMax,
-      minY: -this._profile.dMax,
-      maxY: this._profile.dMax,
-    };
-
-    if( this.label === null ){
-      this.label = this.moldedTube.name;
+    if (this.moldedTube == null) {
+      this.moldedTube = getTube();
     }
-    
-    // attach styles
-    this.styleEl = document.createElement('style');
-    this.styleEl.innerHTML = this.styleText;
-    this.styleEl.setAttribute('id', 'styleEl');
-
-    // create a div as a top-level container
-    this.rootEl = document.createElement('div');
-    this.rootEl.innerHTML = this.templateHTML;
-    this.rootEl.setAttribute('id', 'rootEl');
-
-    // add style and root div to shadow dom
-    this.shadowRoot.appendChild(this.styleEl);
-    this.shadowRoot.appendChild(this.rootEl);
-
-    let scalex = 0.8 * (this.canvas.width) / (this._extents.maxX - this._extents.minX);
-    let scaley = 0.8 / 7.0 * (this.canvas.height) / (this._extents.maxY - this._extents.minY);
-    this._scale = Math.min(scalex, scaley);
-    this._original_scale = this._scale; // scale has units of px/meter
-    this._originx = 0;  // origin x & y are in world units, ie meters
-    this._originy = 0; // origin x & y are in world units, ie meters
-
-    this.canvas.onwheel = this.mouseWheel.bind(this);
-    this.canvas.onmousedown = this.mousedown.bind(this);
-    this.canvas.onmousemove = this.mousemove.bind(this);
-    this.canvas.onmouseup = this.mouseup.bind(this);
-    this.canvas.onmouseout = this.mouseup.bind(this);
-    this.canvas.ondblclick = this.dblclick.bind(this);
-
-    // Begin the animation loop.
-    this.drawProfile();
+    this.label = this.moldedTube ? this.moldedTube.name : "";
+    // console.log("constructor");
   }
 
-  disconnectedCallback() {
-    // browser calls this method when the element is removed from the document
-    // (can be called many times if an element is repeatedly added/removed)
-  }
-
+  /**
+   * The array of attribute names to monitor for changes
+   */
   static get observedAttributes() {
-    /* array of attribute names to monitor for changes */
     return ['label', 'molded-tube'];
   }
 
+  /**
+   * Called before this.connectedCallback() if attributes are present 
+   * in html, and when attributes are changed.
+   */
   attributeChangedCallback(name, oldValue, newValue) {
     // called when one of attributes listed above is modified
     if (name === 'molded-tube') { this.moldedTube = JSON.parse(newValue); }
+    if (name === 'label') { this.label = newValue; }
+    if (this.initialized) this.render();
+    console.log(`"${name}" was "${oldValue}" is now "${newValue}"`);
+  }
+
+  /**
+   *  browser calls this method when the element is added to the document
+   * (can be called many times if an element is repeatedly added/removed)
+   */
+  connectedCallback() {
+
+    if (!(this.initialized)) {
+      // attach styles
+      this.styleEl = document.createElement('style');
+      this.styleEl.setAttribute('id', 'styleEl');
+
+      // create a div as a top-level container
+      this.rootEl = document.createElement('div');
+      this.rootEl.setAttribute('id', 'rootEl');
+
+      this.styleEl.innerHTML = this.styleText;
+      this.rootEl.innerHTML = this.templateHTML;
+
+      // add style and root div to shadow dom
+      this.shadowRoot.appendChild(this.styleEl);
+      this.shadowRoot.appendChild(this.rootEl);
+
+      this.addElementListener(this.canvas, 'wheel', this.mouseWheel.bind(this));
+      this.addElementListener(this.canvas, 'mousedown', this.mousedown.bind(this));
+      this.addElementListener(this.canvas, 'mousemove', this.mousemove.bind(this));
+      this.addElementListener(this.canvas, 'mouseup', this.mouseup.bind(this));
+      this.addElementListener(this.canvas, 'mouseout', this.mouseup.bind(this));
+      this.addElementListener(this.canvas, 'dblclick', this.dblclick.bind(this));
+
+      this.initialized = true;
+    }
+
+    this.render();
+    console.log("connected callback");
+  }
+
+
+
+  /**
+   * browser calls this method when the element is removed from the document
+   * (can be called many times if an element is repeatedly added/removed)
+   */
+  disconnectedCallback() {
+    this.removeAllListeners();
+    this.initialized = false;
   }
 
   /**
    * called when the element is moved to a new document
    * (happens in document.adoptNode, very rarely used)
-   *
-   * @memberof MyElement
    */
   adoptedCallback() {
   }
 
-  get label() {
-    let ret = this.getAttribute('label');
-    return ret;
+  /**
+   * Sets up an event listener, and saves info so all listeners be removed 
+   * easily by calling removeAllListeners()
+   * 
+   * @param {HTMLElement} element the target element
+   * @param {string} eventStr the event to handle
+   * @param {function} listenerFunc event handler
+   */
+  addElementListener(element, eventStr, listenerFunc) {
+    element.addEventListener(eventStr, listenerFunc);
+    this.listeners = this.listeners || [];
+    this.listeners.push({
+      el: element,
+      event: eventStr,
+      listener: listenerFunc
+    });
   }
 
-  set label(str) {
-    console.log(`Property: label was ${this.label} now = ${str}`);
-    if (str && str != '') {
-      this.setAttribute('label', '' + str);
-    } else {
-      this.removeAttribute('label');
-    }
+  /**
+   * Remove all of the listeners added via addElementListener() 
+   */
+  removeAllListeners() {
+    this.listeners.forEach(({ el, event, listener }) => {
+      el.removeEventListner(event, listener);
+    });
+    this.listeners = [];
   }
 
   get canvas() {
-    return this.shadowRoot.querySelector("#profile-canvas");
+    return this.shadowRoot.querySelector("#canvasEl");
   }
 
   get context() {
     return this.canvas.getContext("2d");
   }
 
+  get labelEl() {
+    return this.shadowRoot.querySelector("#labelEl");
+  }
+
+  /**
+   * Get the label text added to the top of the element.
+   * Defaults to the name of the moldedTube.
+   * 
+   * @returns {string}
+   */
+  get label() {
+    return this._label;
+  }
+
+  /**
+   * Set the label text added to the top of the element.
+   * @param {string} str 
+   */
+  set label(str) {
+    this._label = "" + str;
+  }
+
+  /**
+   * Get the MoldedTube object that this element displays.
+   * 
+   * @returns {MoldedTube}
+   */
   get moldedTube() {
     return this._moldedTube;
   }
 
-  set moldedTube(value) {
-    if (value instanceof MoldedTube) {
-      this._moldedTube = value;
+  /**
+   * Set the MoldedTube object that this element displays.
+   * 
+   * @param {MoldedTube} theMoldedTube
+   */
+  set moldedTube(theMoldedTube) {
+    if (theMoldedTube instanceof MoldedTube) {
+      this._moldedTube = theMoldedTube;
     } else {
-      this_moldedTube = new MoldedTube(value);
+      this_moldedTube = new MoldedTube(theMoldedTube);
     }
   }
 
-  /*
-    The style tags allow syntax highlighting, but need to be stripped to
-    work properly because this gets wrapped in <style> </style> tags
-    when added to the shadow dom.
-  */
+  /**
+   * Get the style css as text.
+   * 
+   * @returns {string} the css as a string
+   */
   get styleText() {
+    /* The style tags allow syntax highlighting, but need to be stripped to
+    work properly because this gets wrapped in <style> </style> tags
+    when added to the shadow dom.*/
     let ret = html`
         <style>
           :host {
@@ -162,14 +212,57 @@ class MoldedTubeWC extends HTMLElement {
     return ret;
   }
 
+  /**
+   * Get the HTML to use as a template for this element.
+   * @returns {string}
+   * @readonly
+   */
   get templateHTML() {
     let ret = html`
-      <label id="label">${this.label}</label><br>
-      <canvas id="profile-canvas" height="${this.canvasHeight}" width="${this.canvasWidth}"></canvas>
+      <label id="labelEl"></label><br>
+      <canvas id="canvasEl"></canvas>
     `;
     return ret;
   }
 
+  /**
+   * Prepare the data to draw the element on the screen.
+   * Resets scale to 1:1 and the origin offset to 0,0
+   */
+  render() {
+
+    this.canvas.height = this.canvasHeight;
+    this.canvas.width = this.canvasWidth;
+
+    this.labelEl.textContent = this.label;
+    this._profile = this.moldedTube.profile;
+    this._plySpecs = this.moldedTube.plySpecs;
+
+    // extents is in real worlld units = meters
+    this._extents = {
+      minX: this._profile.xMin,
+      maxX: this._profile.xMax,
+      minY: -this._profile.dMax,
+      maxY: this._profile.dMax,
+    };
+
+    // mouse zooming and panning data
+    this._zoomIntensity = 0.15;
+    let scalex = 0.8 * (this.canvas.width) / (this._extents.maxX - this._extents.minX);
+    let scaley = 0.8 / 7.0 * (this.canvas.height) / (this._extents.maxY - this._extents.minY);
+    this._scale = Math.min(scalex, scaley);
+    this._original_scale = this._scale; // scale has units of px/meter
+
+    this._originx = 0;  // origin x & y are in world units, ie meters
+    this._originy = 0; // origin x & y are in world units, ie meters
+
+    this.drawProfile();
+  }
+
+  /**
+   * Draw the profile and the plies in the canvas element at the current
+   * scale and origin offset
+   */
   drawProfile() {
     const scale = this._scale; // px / meter
     const rngY = this._extents.maxY;  // meters
@@ -222,7 +315,7 @@ class MoldedTubeWC extends HTMLElement {
     // scale the ply widths, so a full wrap is equal in size to the diameter
     let plyWidthScale = 1 / 3.14159;
     // .. or not
-    plyWidthScale = 1.0;
+    //plyWidthScale = 1.0;
 
     // draw each ply
     plySpecs.forEach((plySpec, index) => {
@@ -233,6 +326,15 @@ class MoldedTubeWC extends HTMLElement {
     context.restore();
   }
 
+  /**
+   * Draw a single ply outline, assuming the context has been translated by the
+   * caller, so it will end up in the correct location.
+   * 
+   * @param {PlySpec} plySpec 
+   * @param {Context} ctx 
+   * @param {number} scale 
+   * 
+   */
   drawPly(plySpec, ctx, scale) {
     let start = plySpec.start * scale.x;
     let tStart = plySpec.taperStart * scale.x;
@@ -257,6 +359,12 @@ class MoldedTubeWC extends HTMLElement {
     ctx.restore();
   }
 
+  /**
+   * Handle mouse wheel zooming, so that the point under the mouse remains
+   * stationary while the canvas is zoomed in or out.
+   * 
+   * @param {MouseEvent} event 
+   */
   mouseWheel(event) {
     event.preventDefault();
     const canvas = this.canvas;
@@ -283,6 +391,11 @@ class MoldedTubeWC extends HTMLElement {
     this.drawProfile();
   };
 
+  /**
+   * Listen for mousedown events, to start dragging fo canvas contents.
+   * 
+   * @param {MouseEvent} event 
+   */
   mousedown(event) {
     this._dragging = true;
     this._dragStart = {
@@ -291,6 +404,12 @@ class MoldedTubeWC extends HTMLElement {
     };
   }
 
+  /**
+   * If the mouse moves while dragging is active, update the canvas origin &
+   * redraw the canvas contents.
+   * 
+   * @param {MouseEvent} event 
+   */
   mousemove(event) {
     if (this._dragging) {
       this._dragEnd = {
@@ -302,16 +421,26 @@ class MoldedTubeWC extends HTMLElement {
       this._dragStart = this._dragEnd;
       this._dragEnd = undefined;
       this.drawProfile();
-
     }
   }
 
-  mouseup() {
+  /**
+   * Dragging ends when the user releases the mouse button.
+   * 
+   * @param {MouseEvent} event 
+   */
+  mouseup(event) {
     this._dragging = false;
     this._dragStart = undefined;
     this._dragEnd = undefined;
   }
 
+  /**
+   * Reset the zoom scale and the origin when the user double clicks, &
+   * redraw the canvas contents.
+   * 
+   * @param {MouseEvent} event 
+   */
   dblclick(event) {
     this._scale = this._original_scale;
     this._originx = 0;
